bbf2d6fa3c52bba47f96cc8f9b78e3f4a2af903c
bhargav <bhargav.kaneriya@gmail.com>
2014-02-04 16:54:51 +0530

    Updating users 1

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,14 +10,47 @@ describe "User pages" do
 
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
-  end
+  end #profile page
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end#before
+
+    describe "page" do
+      it { should have_selector('h1',    text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end#page
+
+   describe "with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end#with valid information
+
+      it { should have_selector('title', text: new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { user.reload.name.should  == new_name }
+      specify { user.reload.email.should == new_email }
+    end#with invalid information
+  end#edit
+
 
   describe "signup page" do
     before { visit signup_path }
 
     it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
-  end
+  end#signup page
 
   describe "signup" do
 
